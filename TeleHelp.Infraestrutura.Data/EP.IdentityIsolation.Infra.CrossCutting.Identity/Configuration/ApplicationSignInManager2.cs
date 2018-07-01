632964233f9510561bb327291b93fea3fe9ce516
edsonlima6 <edsonlima6@gmail.com>
2018-07-01 15:29:49 -0300

    Solução real

---
+++ TeleHelp.Infraestrutura.Data/EP.IdentityIsolation.Infra.CrossCutting.Identity/Configuration/ApplicationSignInManager2.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using EP.IdentityIsolation.Infra.CrossCutting.Identity.Model;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+
+namespace EP.IdentityIsolation.Infra.CrossCutting.Identity.Configuration
+{
+    public class ApplicationSignInManager2 : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager2(ApplicationUserManager2 userManager, IAuthenticationManager authenticationManager)
+            : base(userManager, authenticationManager)
+        {
+
+        }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager2)UserManager);                
+        }
+
+        public static ApplicationSignInManager2 Create(IdentityFactoryOptions<ApplicationSignInManager2> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager2(context.GetUserManager<ApplicationUserManager2>(), context.Authentication);
+        }
+    }
+}
---
+++ TeleHelp.Infraestrutura.Data/EP.IdentityIsolation.Infra.CrossCutting.Identity/Context/ApplicationDbContext2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EP.IdentityIsolation.Infra.CrossCutting.Identity.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EP.IdentityIsolation.Infra.CrossCutting.Identity.Context
+{
+    public class ApplicationDbContext2 : IdentityDbContext<ApplicationUser>, IDisposable
+    {
+        public ApplicationDbContext2()
+            : base("MyBI", throwIfV1Schema: false)
+        {
+
+        }
+
+        public static ApplicationDbContext2 Create()
+        {
+            return new ApplicationDbContext2();
+        }
+    }
+}
